74b18d611248fd38ccf7d3ea33ff0735f33312cf
Harrisson Ricardo Biaggio <harrissonbiaggio@gmail.com>
2019-07-20 17:05:56 -0300

    feat: new functions admin

--- simplemock/curse/admin.py
+++ simplemock/curse/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Curse
 
-# Register your models here.
+class CurseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'start_date']
+    search_fields = ['name']
+
+admin.site.register(Curse, CurseAdmin)
--- simplemock/curse/models.py
+++ simplemock/curse/models.py
@@ -5,9 +5,17 @@ class Curse(models.Model):
     slug = models.SlugField('Atalho')
     description = models.TextField('Descrição', blank=True)
     start_date = models.DateField('Data inicio', blank=True, null=True)
-    image = models.ImageField('Imagem', upload_to='curses/image')
+    image = models.ImageField('Imagem', upload_to='curses/image', blank=True, null=True)
     created_at = models.DateTimeField('Criaçao', auto_now_add=True)
     updated_at = models.DateTimeField('Alteracao', auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ["name"]
     
     
     
